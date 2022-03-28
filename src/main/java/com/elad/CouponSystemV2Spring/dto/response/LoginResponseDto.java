523ddab68b494def595a61e50a035cf65bab3c27
eladbarzilay <eladbarzilay53@gmail.com>
2022-03-28 22:52:33 +0300

    l

--- src/main/java/com/elad/CouponSystemV2Spring/dto/response/LoginResponseDto.java
+++ src/main/java/com/elad/CouponSystemV2Spring/dto/response/LoginResponseDto.java
@@ -1,14 +1,12 @@
 package com.elad.CouponSystemV2Spring.dto.response;
 
-import com.elad.CouponSystemV2Spring.login.ClientType;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.UUID;
 
